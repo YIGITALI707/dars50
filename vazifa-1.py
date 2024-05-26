80536e02ac4f6ae8e5b400e10c12a5c6a552b6ef
YIGITALI707 <alibek1998ali12345@gmail.com>
2024-05-26 23:21:51 +0300

    first commit

--- vazifa-1.py
+++ vazifa-1.py
@@ -1,15 +1,8 @@
-from datetime import datetime
-class Bugun:
-    def __init__(self,datetime):
-       self.datetime=datetime
-    def day(self):
-        return self.datetime.now().day
-    def month(self):
-        return self.datetime.now().month
-    def year(self):
-        return self.datetime.now().year
-a=Bugun(datetime)
-print(a.day())
+l=[1,2,3,4,5,6,7,17,9,10]
+for i in range(len(l)-1):
+    if l[i]+1!=l[i+1]:
+        print(f"noto'g'ri raqamlar {l[i]}")
+
 
 
 
--- vazifa50.py
+++ vazifa50.py
@@ -1,1 +1,36 @@
-def txt_fayl_ochuvchi():
+
+
+#1-masala
+txt_fayl_joylashuvi="D:\new project\vazifa50.txt"
+def txt_fayl_ochuvchi(txt_fayl_joylashuvi,):
+    try:
+        x=open(r"D:\\new project\\vazifa50.txt","r")
+        a=x.read()
+        return a
+    except FileNotFoundError:
+        print(f"Fayl {txt_fayl_joylashuvi} yo'q ekan.")
+txt_fayl_joylashuvi=r"D:\\new project\\vazifa50.txt"
+print(txt_fayl_ochuvchi(txt_fayl_joylashuvi)) # txt fayl o'qiy oladigan fayl tuzdim lekin txt fayl ichidagi
+# sozlarni qanday qilib sanashni bilmadim???????????
+
+#2-masala
+def txt_fayl_ochuvchi(txt_fayl_joylashuvi):
+    try:
+        x=open(r"D:\\new project\\vazifa50.txt","r")
+        u=x.read()
+        b = eval(u)
+        c=max(b,key=len)
+        return u,c
+    except FileNotFoundError:
+        print(f"Fayl {txt_fayl_joylashuvi} yo'q ekan.")
+
+txt_fayl_joylashuvi=r"D:\\new project\\vazifa50.txt"
+print(txt_fayl_ochuvchi(txt_fayl_joylashuvi))
+
+#3-masala
+l=[1,2,3,4,5,6,7,17,9,10]
+for i in range(len(l)-1):
+    if l[i]+1!=l[i+1]:
+        print(f"noto'g'ri raqamlar {l[i]}")
+
+
